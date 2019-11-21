3f366c1caaadae55627f90a72e6cb2747fc542e3
Daniel Pichardo <danielpichardo716@gmail.com>
2019-11-21 01:24:53 -0500

    implement order submission in contactdata

--- burger-builder-basic/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-basic/src/containers/Checkout/ContactData/ContactData.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
   state = {
@@ -10,20 +12,54 @@ class ContactData extends Component {
     address:  {
       street: '',
       postalCode: ''
+    },
+    loading: false
+  }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: 'Dan P',
+        address: {
+          street: 'TestStreet 1',
+          zipCode: '43265',
+          country: 'USA'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
     }
+    axios.post('/orders.json', order)
+      .then(response => {
+        this.setState({loading: false});
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        this.setState({loading: false});
+      });
   }
 
   render() {
+    let form = (
+      <form>
+        <input type="text" name="name" placeholder="Your Name" />
+        <input type="text" name="email" placeholder="Your Email" />
+        <input type="text" name="street" placeholder="Street" />
+        <input type="text" name="postal" placeholder="Postal Code" />
+        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+      </form>
+    );
+    if(this.state.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter your Contact Information</h4>
-        <form>
-          <input type="text" name="name" placeholder="Your Name" />
-          <input type="text" name="email" placeholder="Your Email" />
-          <input type="text" name="street" placeholder="Street" />
-          <input type="text" name="postal" placeholder="Postal Code" />
-          <Button btnType="Success">ORDER</Button>
-        </form>
+        {form}
       </div>
     )
   }
