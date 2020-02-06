f5cb5c2b0cdd74a041c80da70d52d11f0226759e
Daniel Pichardo <danielpichardo716@gmail.com>
2020-02-06 00:01:56 -0500

    Add two way binding for form inputs and dropdown

--- burger-builder-basic/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-basic/src/containers/Checkout/ContactData/ContactData.js
@@ -79,6 +79,19 @@ class ContactData extends Component {
       });
   }
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm});
+  }
+
   render() {
     const formElementsArray = [];
     for(let key in this.state.orderForm) {
@@ -91,10 +104,11 @@ class ContactData extends Component {
       <form>
         {formElementsArray.map(formElement => (
           <Input 
-            ket={formElement.id}
+            key={formElement.id}
             elementType={formElement.config.elementType} 
             elementConfig={formElement.config.elementConfig}
-            value={formElement.config.value}/>
+            value={formElement.config.value}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}        
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>
